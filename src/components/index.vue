62b0c5466671181264af88c7a333ce575bc218cd
xizhjxust <253615105@qq.com>
2019-02-15 10:14:06 +0800

    toggleMenu
    toggleMenu

---
+++ src/components/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="toggle_menu_container">
+    <toggle-menu
+      positon="LT"
+      menuBg="white"
+      itmeBg="white"
+      :menuItems="menuItems"
+      @clickMenu="clickMenu"
+      baseDistacne="150"
+    ></toggle-menu>
+  </div>
+</template>
+<script>
+import toggleMenu from './toggleMenu'
+export default {
+  name: 'index',
+  data() {
+    return {
+      menuItems: [
+        { name: 'menu1', src: require('../assets/emoji.png') },
+        { name: 'menu2', src: require('../assets/cart.png') },
+        { name: 'menu3', src: require('../assets/folder.png') },
+        { name: 'menu4', src: require('../assets/home.png') },
+        { name: 'menu5', src: require('../assets/my.png') }
+      ]
+    }
+  },
+  methods: {
+    clickMenu(item, index) {
+      console.log(item, index)
+    }
+  },
+  components: {
+    toggleMenu
+  }
+}
+</script>
+<style scoped>
+</style>
+
